6c39f403a0ddc70aceba6940152f1ee46a46220b
Artem Novikov <novikov711@gmail.com>
2022-05-11 22:26:33 +0000

    Fixed the code so it looks through an array vertically and horizontally

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -34,5 +34,26 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+    
   });
+    it("should return true if the word is present vertically", function() {
+      const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+    assert.isTrue(result)
+  })
+
+  it("should return an empty array if the letters is an empty array", function() {
+    const result = wordSearch([], 'FRANK') 
+    assert.isTrue(Array.isArray(result) && result.length === 0);
+  })
+
 });
